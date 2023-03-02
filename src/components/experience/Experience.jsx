6ac1d08dcba6e2703fdc2db7647920f3e1ce5896
ashen8981 <ashenmalindu112@gmail.com>
2023-03-02 17:03:40 +0530

    basic structure is done

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './experience.css'
+
+function Experience() {
+  return (
+    <div>experience</div>
+  )
+}
+
+export default Experience
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './portfolio.css'
+
+function Portfolio() {
+  return (
+    <div>portfolio</div>
+  )
+}
+
+export default Portfolio
