b39dc2278864d4d43ce09a99ce017ca755cc99a2
ashen8981 <ashenmalindu112@gmail.com>
2023-03-06 15:00:16 +0530

    all errors fixed now.....

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './footer.css'
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='ft'>
+      <a href='#' className='footer__logo'>ASHEN MALINDU</a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Projects</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className="footer__coyright">
+        <small>
+        &copy; 2023 Ashen Malindu. All rights reserved.
+          </small>
+      </div>
+    </footer>
   )
 }
 
