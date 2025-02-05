ad839236f6ca1106421699f2d8ec687586f7b2b2
Ashen Malindu <ashenmalindu112@gmail.com>
2025-02-05 15:02:50 +0530

    splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
-import './app.css'; 
 import './splashScreen.css'; 
 
 const loadingTexts = ["Loading...", "Please Wait...", "Getting Ready..."];
