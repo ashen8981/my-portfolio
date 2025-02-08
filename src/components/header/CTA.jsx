56409147c4aab754fb11c577f07b5ea6e6afbe4a
Ashen Malindu <ashenmalindu112@gmail.com>
2025-02-08 21:31:29 +0530

    aos in buttons

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,10 +4,23 @@ import CV from '../../assets/ashen_malindu_cv.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        <a 
+          href={CV} 
+          download 
+          className='btn' 
+          data-aos="fade-right"
+        >
+          Download CV
+        </a>
+        <a 
+          href='#contact' 
+          className='btn btn-primary' 
+          data-aos="fade-left"
+        >
+          Let's Talk
+        </a>
     </div>
   )
 }
 
-export default CTA
+export default CTA;
