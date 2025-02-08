65bffd733c41e857b53760a0ed7494f26663cd04
Ashen Malindu <ashenmalindu112@gmail.com>
2025-02-08 22:34:36 +0530

    aos

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/ABOUT_ME.jpg'
-
-
+import React from 'react';
+import './about.css';
+import ME from '../../assets/ABOUT_ME.jpg';
 
 function About() {
   return (
     <section id='about'>
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+      <h5 data-aos="fade-up">Get To Know</h5>
+      <h2 data-aos="fade-up" data-aos-delay="100">About Me</h2>
       <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={ME} alt='About Image'/>
+        <div className="about__me" data-aos="zoom-in" data-aos-delay="200">
+          <div className="about__me-image" data-aos="rotate" data-aos-delay="300">
+            <img src={ME} alt='About Me' />
           </div>
         </div>
-          <div className="about__content">
-            <p>
+        <div className="about__content" data-aos="fade-left" data-aos-delay="400">
+          <p>
             I am a Software Engineer, a graduate of the Institute of Technology, University of Moratuwa, with a strong passion for full-stack development. 
             I am eager to gain hands-on experience while applying my knowledge and skills to real-world projects. 
-            My goal is to contribute effectively to a dynamic team and continuously grow as a developer, delivering my best to the company.               
-            </p>
-            <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-          </div>
+            My goal is to contribute effectively to a dynamic team and continuously grow as a developer, delivering my best to the company.
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,61 +1,72 @@
-import React from 'react'
-import './contact.css'
-import {MdEmail} from 'react-icons/md'
-import {FaFacebookMessenger} from 'react-icons/fa'
-import {RiWhatsappFill} from 'react-icons/ri'
-import { useRef } from 'react';
-import emailjs from 'emailjs-com'
+import React, { useRef } from 'react';
+import './contact.css';
+import { MdEmail } from 'react-icons/md';
+import { FaFacebookMessenger } from 'react-icons/fa';
+import { RiWhatsappFill } from 'react-icons/ri';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_d806n3c', 'template_nn6cb6j', form.current, 'yvcO6PXU_OhBLAF23')
-     e.target.reset()
+    emailjs.sendForm(
+      'service_d806n3c',
+      'template_nn6cb6j',
+      form.current,
+      'yvcO6PXU_OhBLAF23'
+    );
+    e.target.reset();
   };
 
-
   return (
-    <section id='contact'>
-      <h5 className='ex123'>Get In Touch</h5>
-      <h2>Contact Me</h2>
+    <section id="contact">
+      <h5 className="ex123" data-aos="fade-down">Get In Touch</h5>
+      <h2 data-aos="fade-up">Contact Me</h2>
+
       <div className="container contact__container">
         <div className="contact__options">
-          <article className="contact__option">
-            <MdEmail className='contact__option-icon'/>
+          <article className="contact__option" data-aos="fade-right">
+            <MdEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>ashenmalindu112@gmail.com</h5>
-            <a href='mailto:ashenmalindu112@gmail.com' target={'_blank'}>Send a message</a>
+            <a href="mailto:ashenmalindu112@gmail.com" target="_blank" rel="noopener noreferrer">
+              Send a message
+            </a>
           </article>
 
-          <article className='contact__option'>
-            <FaFacebookMessenger className='contact__option-icon'/>
+          <article className="contact__option" data-aos="fade-up">
+            <FaFacebookMessenger className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Ashen Malindu</h5>
-            <a href='https://m.me/malindu.ashen'target={'_blank'}>Send a message</a>
+            <a href="https://m.me/malindu.ashen" target="_blank" rel="noopener noreferrer">
+              Send a message
+            </a>
           </article>
 
-          <article className='contact__option'>
-            <RiWhatsappFill className='contact__option-icon'/>
+          <article className="contact__option" data-aos="fade-left">
+            <RiWhatsappFill className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+94718794279</h5>
-            <a href="https://wa.me/94718794279" target={'_blank'}>Send a message</a>
+            <a href="https://wa.me/94718794279" target="_blank" rel="noopener noreferrer">
+              Send a message
+            </a>
           </article>
-
         </div>
-        {/*End of contact options*/}
-      <form ref={form} onSubmit={sendEmail}>
-        <input type="text" name='name' placeholder='Your Full Name*' required />
-        <input type="email" name='email' placeholder='Your Email*' required />
-        <textarea name ='message' rows='7' placeholder='Your Message *' required/>
-        <button type='submit' className='btn btn-primary'>Send Message</button>
-      </form>
+        {/* End of Contact Options */}
+
+        <form ref={form} onSubmit={sendEmail} data-aos="zoom-in">
+          <input type="text" name="name" placeholder="Your Full Name*" required />
+          <input type="email" name="email" placeholder="Your Email*" required />
+          <textarea name="message" rows="7" placeholder="Your Message *" required />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
