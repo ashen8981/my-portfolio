7f98be61b7146192523ed3b4b1f03f3f7d4f0625
Ashen Malindu <ashenmalindu112@gmail.com>
2025-02-08 22:38:36 +0530

    aos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,74 +1,77 @@
-import React from 'react'
-import './portfolio.css'
-import ITUM from '../../assets/ITUM.png'
-import VOTE from '../../assets/VOTE.png'
-import COFFEE from '../../assets/COFFEE.png'
-import FUEL from '../../assets/FUEL.png'
-import CAFE from '../../assets/CAFE.png'
-import BLOG from '../../assets/BLOG.png'
-
+import React from 'react';
+import './portfolio.css';
+import 'aos/dist/aos.css';
+import ITUM from '../../assets/ITUM.png';
+import VOTE from '../../assets/VOTE.png';
+import COFFEE from '../../assets/COFFEE.png';
+import FUEL from '../../assets/FUEL.png';
+import CAFE from '../../assets/CAFE.png';
+import BLOG from '../../assets/BLOG.png';
 
 function Portfolio() {
   return (
-    <section id='portfolio'>
-      <h5 className='ex12'>My Recent Projects</h5>
-      <h2>Projects</h2>
+    <section id="portfolio">
+      <h5 className="ex12" data-aos="fade-down">My Recent Projects</h5>
+      <h2 data-aos="fade-up">Projects</h2>
       <div className="container portfolio__container">
-        <article className='porfolio__item'>
+        
+        <article className="porfolio__item" data-aos="zoom-in">
           <div className="portfolio__item-image">
-            <img src={VOTE} alt=''/>
-            </div>
-            <h3>Vote App</h3>
-            <h5>Flutter,Firebase</h5>
-            <a href='https://github.com/ashen8981/Poll_App.git' className='btn' target='_blank'>GitHub</a>
+            <img src={VOTE} alt=""/>
+          </div>
+          <h3>Vote App</h3>
+          <h5>Flutter, Firebase</h5>
+          <a href="https://github.com/ashen8981/Poll_App.git" className="btn" target="_blank">GitHub</a>
         </article>
-        <article className='porfolio__item'>
+
+        <article className="porfolio__item" data-aos="flip-left">
           <div className="portfolio__item-image">
-            <img src={ITUM} alt=''/>
-            </div>
-            <h3>ITUM Communication Platform</h3>
-            <h5>Flutter,Firebase</h5>
-            <a href='https://github.com/ashen8981/ITUM_Communication_Platform_V3.git' className='btn' target='_blank'>GitHub</a>
+            <img src={ITUM} alt=""/>
+          </div>
+          <h3>ITUM Communication Platform</h3>
+          <h5>Flutter, Firebase</h5>
+          <a href="https://github.com/ashen8981/ITUM_Communication_Platform_V3.git" className="btn" target="_blank">GitHub</a>
         </article>
-        <article className='porfolio__item'>
+
+        <article className="porfolio__item" data-aos="fade-up">
           <div className="portfolio__item-image">
-            <img src={COFFEE} alt=''/>
-            </div>
-            <h3>COFFEE APP</h3>
-            <h5>Flutter</h5>
-            <a href='https://github.com/ashen8981/Coffe_UI.git' className='btn' target='_blank'>GitHub</a>
+            <img src={COFFEE} alt=""/>
+          </div>
+          <h3>COFFEE APP</h3>
+          <h5>Flutter</h5>
+          <a href="https://github.com/ashen8981/Coffe_UI.git" className="btn" target="_blank">GitHub</a>
         </article>
-        <article className='porfolio__item'>
+
+        <article className="porfolio__item" data-aos="zoom-in">
           <div className="portfolio__item-image">
-            <img src={FUEL} alt=''/>
-            </div>
-            <h3>Fuel Management APP</h3>
-            <h5>Flutter</h5>
-            <a href='https://github.com/ashen8981/Fuel_App.git' className='btn' target='_blank'>GitHub</a>
+            <img src={FUEL} alt=""/>
+          </div>
+          <h3>Fuel Management APP</h3>
+          <h5>Flutter</h5>
+          <a href="https://github.com/ashen8981/Fuel_App.git" className="btn" target="_blank">GitHub</a>
         </article>
-        <article className='porfolio__item'>
+
+        <article className="porfolio__item" data-aos="flip-right">
           <div className="portfolio__item-image">
-            <img src={CAFE} alt=''/>
-            </div>
-            <h3>Perkup Cafe Website</h3>
-            <h5>React JS, Vite, Tailwind CSS</h5>
-            <a href="https://perkupcafe1.netlify.app/" className="btn" target="_blank" rel="noopener noreferrer">
-            Visit Website
-            </a>
+            <img src={CAFE} alt=""/>
+          </div>
+          <h3>Perkup Cafe Website</h3>
+          <h5>React JS, Vite, Tailwind CSS</h5>
+          <a href="https://perkupcafe1.netlify.app/" className="btn" target="_blank" rel="noopener noreferrer">Visit Website</a>
         </article>
-        <article className='porfolio__item'>
+
+        <article className="porfolio__item" data-aos="fade-up">
           <div className="portfolio__item-image">
-            <img src={BLOG} alt=''/>
-            </div>
-            <h3>Blog Site</h3>
-            <h5>React Js, Express, Node Js, MongoDB</h5>
-            <a href="https://github.com/ashen8981/blog_website_MERN.git" className="btn" target="_blank" rel="noopener noreferrer">
-            GitHub
-            </a>
+            <img src={BLOG} alt=""/>
+          </div>
+          <h3>Blog Site</h3>
+          <h5>React Js, Express, Node Js, MongoDB</h5>
+          <a href="https://github.com/ashen8981/blog_website_MERN.git" className="btn" target="_blank" rel="noopener noreferrer">GitHub</a>
         </article>
+        
       </div>
     </section>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,61 +1,61 @@
-import React from 'react'
-import './services.css'
-import {SiFlutter} from 'react-icons/si'
-import {CgWebsite} from 'react-icons/cg'
-import {TbSlideshow} from 'react-icons/tb'
-
+import React from 'react';
+import 'aos/dist/aos.css';
+import './services.css';
+import { SiFlutter } from 'react-icons/si';
+import { CgWebsite } from 'react-icons/cg';
+import { TbSlideshow } from 'react-icons/tb';
 
 function Services() {
   return (
-    <section id='services'>
-      <h5 className='ex'>Here is my</h5>
-      <h2>Application Services</h2>
+    <section id="services">
+      <h5 className="ex" data-aos="fade-up">Here is my</h5>
+      <h2 data-aos="fade-up" data-aos-delay="100">Application Services</h2>
+
       <div className="container services__container">
-        <article className='service'>
+        <article className="service" data-aos="fade-right" data-aos-delay="200">
           <div className="service__head">
             <h3>Mobile App Development</h3>
           </div>
-          <div className='service__icon'>
-          <SiFlutter />
+          <div className="service__icon">
+            <SiFlutter />
           </div>
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <p>I have knowledge about the Flutter,Firebase mobile app development.</p>
+              <p>Experienced in Flutter, Firebase, Kotlin, and MySQL for mobile app development.</p>
             </li>
           </ul>
         </article>
-        { /* end of mobile development*/}
-        <article className='service'>
+
+        <article className="service" data-aos="fade-up" data-aos-delay="300">
           <div className="service__head">
             <h3>Web Development</h3>
           </div>
-          <div className='service__icon'>
-          <CgWebsite/> 
+          <div className="service__icon">
+            <CgWebsite />
           </div>
-          <ul className='service__list'>
+          <ul className="service__list">
             <li>
-              <p> I have knowledge about the HTML/CSS/JavaScript and React framework.</p>
+              <p>I have knowledge about HTML/CSS/JavaScript and the React framework.</p>
             </li>
           </ul>
         </article>
-        { /* end of Web development*/}
-        <article className='service'>
+
+        <article className="service" data-aos="fade-left" data-aos-delay="400">
           <div className="service__head">
-              <h3>UI/UX Design</h3> 
-            </div>
-            <div className='service__icon'>
-            <TbSlideshow/>
-            </div>
-            <ul className='service__list'>
+            <h3>UI/UX Design</h3>
+          </div>
+          <div className="service__icon">
+            <TbSlideshow />
+          </div>
+          <ul className="service__list">
             <li>
-              <p>I have worked with Adobe XD,Adobe Photoshop and Adobe illustrator in my university period.</p>
+              <p>I have worked with Adobe XD, Photoshop, and Illustrator during my university period.</p>
             </li>
           </ul>
         </article>
-        { /*UI/UX Design*/}
       </div>
-      </section>
-  )
+    </section>
+  );
 }
 
-export default Services
+export default Services;
