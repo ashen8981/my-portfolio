eef075fe3f7b327b916e648a4a7d8e2361685e0a
Ashen Malindu <ashenmalindu112@gmail.com>
2025-02-08 21:34:42 +0530

    aos with code enhancements

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import {ImLinkedin} from 'react-icons/im'
-import {BsGithub} from 'react-icons/bs'
-import {BsFacebook} from 'react-icons/bs'
- 
+import React from 'react';
+import { ImLinkedin } from 'react-icons/im';
+import { BsGithub } from 'react-icons/bs';
+import { BsFacebook } from 'react-icons/bs';
+import './../../styles/aos/aosStyles.css';
 
 function HeaderSocials() {
   return (
     <div className='header__socials'>
-        <a href='' target='_blank'></a>
-        <a href='https://www.linkedin.com/in/ashen-malindu-a07613208/' target='_blank'><ImLinkedin/></a>
-        <a href='https://github.com/ashen8981' target='_blank'><BsGithub/></a>
-        <a href='https://www.facebook.com/malindu.ashen' target='_blank'><BsFacebook/></a>
+      <a
+        href='https://www.linkedin.com/in/ashen-malindu-a07613208/'
+        target='_blank'
+        rel='noreferrer'
+        data-aos="fade-left"
+      >
+        <ImLinkedin />
+      </a>
+      <a
+        href='https://github.com/ashen8981'
+        target='_blank'
+        rel='noreferrer'
+        data-aos="fade-up"
+      >
+        <BsGithub />
+      </a>
+      <a
+        href='https://www.facebook.com/malindu.ashen'
+        target='_blank'
+        rel='noreferrer'
+        data-aos="fade-right"
+      >
+        <BsFacebook />
+      </a>
     </div>
-  )
+  );
 }
 
-export default HeaderSocials
+export default HeaderSocials;
