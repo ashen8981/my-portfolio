80f04fa9076d6ee276c3a7925397bc7e6b7e8042
Ashen Malindu <ashenmalindu112@gmail.com>
2025-02-09 08:57:36 +0530

    aos in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import './footer.css'
+import React, { useEffect } from 'react';
+import './footer.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Footer = () => {
   return (
-    <footer className='ft'>
-      <a href='#' className='footer__logo'>ASHEN MALINDU</a>
-      <ul className='permalinks'>
+    <footer className='ft' data-aos="fade-up" data-aos-duration="1000">
+      <a href='#' className='footer__logo' data-aos="fade-up" data-aos-delay="200">ASHEN MALINDU</a>
+
+      <ul className='permalinks' data-aos="fade-up" data-aos-delay="400">
         <li><a href='#'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
@@ -13,13 +16,12 @@ const Footer = () => {
         <li><a href='#portfolio'>Projects</a></li>
         <li><a href='#contact'>Contact</a></li>
       </ul>
-      <div className="footer__coyright">
-        <small>
-        &copy; 2025 Ashen Malindu. All rights reserved.
-          </small>
+
+      <div className="footer__coyright" data-aos="fade-up" data-aos-delay="600">
+        <small>&copy; 2025 Ashen Malindu. All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
